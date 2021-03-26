76fc50d89f0d37a3b729d2b3e3a7a7057be616e5
rushabh <rushah101@gmail.com>
2021-03-26 23:20:06 +0530

    Harsh's UI integrated

--- app/src/main/java/com/example/devsochackathon/MainActivity.java
+++ app/src/main/java/com/example/devsochackathon/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
@@ -62,5 +65,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+//        Button but1 = findViewById(R.id.button1);
+//        but1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, MazeActivity.class);
+//                view.getContext().startActivity(intent);
+//            }
+//        });
+    }
+
+    public void setMaze(View view) {
+        Intent intent = new Intent(MainActivity.this, MazeActivity.class);
+        startActivity(intent);
+    }
+
+    public void setHack(View view) {
+        Intent intent = new Intent(MainActivity.this, MainMenu.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/devsochackathon/MazeFragment.java
+++ app/src/main/java/com/example/devsochackathon/MazeFragment.java
@@ -1,5 +1,6 @@
 package com.example.devsochackathon;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,4 +63,6 @@ public class MazeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_maze, container, false);
     }
+
+
 }
