88b296c5c839bfaecdaf1898cb82b203f92acc3b
rushabh <rushah101@gmail.com>
2021-03-26 22:28:54 +0530

    start puzzle intent from another activity

--- app/src/main/java/com/example/devsochackathon/MainActivity.kt
+++ app/src/main/java/com/example/devsochackathon/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.devsochackathon
 
-import android.app.AlertDialog
-import android.graphics.Color
-import android.media.Image
+import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
-import android.widget.ImageView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     var pipes = PipeLand()
@@ -15,120 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Sets all the pipe images at the start
-        for (i in 0..7) {
-            for (j in 0..5) {
-                setPipe(i, j)
-            }
+        var but = findViewById<Button>(R.id.button)
+        but.setOnClickListener{
+            val intent: Intent = Intent(this, MazeActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    // Called when a pipe is clicked
-    fun pipeClick(view: View) {
-
-        val iview = findViewById<ImageView>(view.id)
-        iview.setColorFilter(Color.argb(255, 85, 188, 223))
-
-        val pipeId = pipes.swap(resources.getResourceEntryName(view.id))
-        if (pipeId == 0) {
-            swapPipes()
-        }
-        else if(pipeId == 2) {
-            errorPipes()
-        }
-        if (pipes.checkForPath()) finishDialog()
-    }
-
-    // Re/sets the pipe image
-    fun setPipe(i: Int, j: Int) {
-
-        val pipeViewId = "imageView" + (i + 1) + (j + 1)
-        val iview = findViewById<ImageView>(resources.getIdentifier(pipeViewId, "id", packageName))
-        when (pipes.layout[i]!![j]) {
-            0 -> iview.setImageResource(R.drawable.ic_bentpipe1)
-            1 -> iview.setImageResource(R.drawable.ic_bentpipe2)
-            2 -> iview.setImageResource(R.drawable.ic_bentpipe3)
-            3 -> iview.setImageResource(R.drawable.ic_bentpipe4)
-            4 -> iview.setImageResource(R.drawable.ic_strpipe1)
-            5 -> iview.setImageResource(R.drawable.ic_strpipe2)
-        }
-    }
-
-    // Visuals for pipe swap
-    fun swapPipes() {
-        val pipeString1 = "imageView" + (pipes.i1 + 1) + (pipes.j1 + 1)
-        val iview1 = findViewById<ImageView>(resources.getIdentifier(pipeString1, "id", packageName))
-        iview1.clearColorFilter()
-
-        val pipeString2 = "imageView" + (pipes.i2 + 1) + (pipes.j2 + 1)
-        val iview2 = findViewById<ImageView>(resources.getIdentifier(pipeString2, "id", packageName))
-        iview2.clearColorFilter()
-
-        val animLength: Long = 250
-
-        iview1.animate().apply {
-            duration = animLength
-            scaleX(0.1f)
-            scaleY(0.1f)
-        }.withEndAction {
-            setPipe(pipes.i1, pipes.j1)
-            iview1.animate().apply {
-                duration = animLength
-                scaleX(1f)
-                scaleY(1f)
-            }
-        }.start()
-
-        iview2.animate().apply {
-            duration = animLength
-            scaleX(0.1f)
-            scaleY(0.1f)
-        }.withEndAction {
-            setPipe(pipes.i2, pipes.j2)
-            iview2.animate().apply {
-                duration = animLength
-                scaleX(1f)
-                scaleY(1f)
-            }
-
-        }.start()
-    }
-
-    fun errorPipes() {
-        val pipeString1 = "imageView" + (pipes.i1 + 1) + (pipes.j1 + 1)
-        val iview1 = findViewById<ImageView>(resources.getIdentifier(pipeString1, "id", packageName))
-
-        val pipeString2 = "imageView" + (pipes.i2 + 1) + (pipes.j2 + 1)
-        val iview2 = findViewById<ImageView>(resources.getIdentifier(pipeString2, "id", packageName))
-
-        val animLength: Long = 250
-
-        iview1.animate().apply {
-            duration = animLength
-            iview1.setColorFilter(Color.argb(255, 255, 0, 0))
-        }.withEndAction {
-            iview1.clearColorFilter()
-        }.start()
-
-        iview2.animate().apply {
-            duration = animLength
-            iview2.setColorFilter(Color.argb(255, 255, 0, 0))
-        }.withEndAction {
-            iview2.clearColorFilter()
-        }.start()
-    }
-
-    // Runs when pipe puzzle completed
-    fun finishDialog() {
-        val alertDialog = AlertDialog.Builder(this@MainActivity).create()
-        alertDialog.setTitle("Congrats!")
-        alertDialog.setMessage("You did it!!")
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK"
-        ) { dialog, which -> dialog.dismiss() }
-        alertDialog.show()
-    }
-
-    fun restartButton(view: View, event: MotionEvent) {
 
     }
+
+//    fun setMaze(view: View) {
+//        val intent = Intent(this, MazeActivity::class.java)
+//        startActivity(intent)
+//    }
 }
